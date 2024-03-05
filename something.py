8ecd4aefaf47501a57dbe3a74f9683a7e2bbabb4
Brent Soliman <shebna@Brents-Computer.local>
2024-03-05 12:46:59 -0700

    commit

--- something.py
+++
@@ -1,7 +0,0 @@
-import download_image
-
-var = download_image.Download_Image()
-
-var.getImage('https://cdn.discordapp.com/attachments/1154534799986405489/1157007414797807616/IMG_6219.png?ex=65170a9f&is=6515b91f&hm=d9290f19a1d45d2bac62dc1e2a8a22a5acedf8ca3c14d1441ed6a6ec50a5edec&')
-
-#var.removeImage()
